e1709ed9c8e23d566aa2c8d3f70345c3149bd308
hirouniverse <hiro.kikuchi.universe@gmail.com>
2020-11-12 11:50:30 +0700

    fix rewrite common page component
    rewrite common page component with tsx

---
+++ frontend/next-typescript/pages/_app.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react'
+import { AppProps } from 'next/app'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
